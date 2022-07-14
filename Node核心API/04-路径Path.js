cae01ebe750baf0efde05c2d815b683ead247222
AzrDream <1499325862@qq.com>
2022-07-14 15:42:05 +0800

    NodeJS核心API-path其他方法

--- Node核心API/04-路径Path.js
+++ Node核心API/04-路径Path.js
@@ -16,3 +16,27 @@ console.log(res3, res4, res5);
 
 console.log(path.sep);
 console.log(path.delimiter);
+
+let obj = path.parse("/a/b/c/d/index.html");
+console.log(obj);
+
+let obj2 = {
+    root: '/',
+    dir: '/a/b/c/d',
+    base: 'index.html',
+    ext: '.html',
+    name: 'index'
+};
+let str = path.format(obj2);
+console.log(str);
+
+let str2 = path.join("/a/b","/c","../");
+console.log(str2);
+
+let res6 = path.normalize("/a//b//c/d////index.html");
+console.log(res6);
+res6 = path.relative("/a/b/c/d/index.html","/a/b/e/f");
+console.log(res6);
+
+res6 = path.resolve('/foo/bar','/baz');
+console.log(res6);
